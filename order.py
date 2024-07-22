19e9fa33d9aef432f54a4d1c193b1bc715096076
tobias lust <tobiaslust.dev@gmail.com>
2024-07-22 16:33:35 -0300

    modify order gui

--- order.py
+++ order.py
@@ -3,49 +3,36 @@ from tkinter import messagebox
 
 
 def order_gui() -> dict:
-    """Interfaz grafica para que  el usuario pueda agregar un pedido a la factura
-    contiene entrys para cantidad,producto,precio,subtotal.
+    """Interfaz grafica para que el usuario pueda agregar un pedido a la factura
+    contiene entrys para cantidad, producto, precio, subtotal.
 
     Returns:
         dict: {cantidad: int, producto: str, precio: float, subtotal: float}
     """
-    # Init
+    # Inicialización de la ventana principal
     window = tk.Tk()
-    # Definición de resolución y que no sea ajustable la ventana
     window.geometry("800x600")
     window.resizable(False, False)
 
-    # Cliente entry
-    cantidad = tk.Label(window, text="Cantidad")
-    cantidad.grid(row=0, column=0, padx=10, pady=5)
+    # Definición de los widgets de entrada y sus etiquetas
+    tk.Label(window, text="Cantidad").grid(row=0, column=0, padx=10, pady=5)
     cantidad_ent = tk.Entry(window)
-    # cantidad_ent.insert(0,0)
     cantidad_ent.grid(row=0, column=1)
 
-    # Producto entry
-    producto = tk.Label(window, text="Producto")
-    producto.grid(row=1, column=0, padx=10, pady=5)
+    tk.Label(window, text="Producto").grid(row=1, column=0, padx=10, pady=5)
     producto_ent = tk.Entry(window)
     producto_ent.grid(row=1, column=1)
 
-    # Precio entry
-    precio = tk.Label(window, text="Precio")
-    precio.grid(row=2, column=0, padx=10, pady=5)
+    tk.Label(window, text="Precio").grid(row=2, column=0, padx=10, pady=5)
     precio_ent = tk.Entry(window)
-    # precio_ent.insert(0,0)
     precio_ent.grid(row=2, column=1)
 
-    # Subtotal
     subtotal = tk.Label(window, text=f"Subtotal: 0.0")
     subtotal.grid(row=2, column=3, padx=10, pady=5)
 
     def actualizar_subtotal(event=None):
         """Actualizar el valor de "subtotal" intentando convertir el valor de
-        cantidad_val a int y precio_val a float.
-
-        Args:
-            event Defaults to None.
-        """
+        cantidad_val a int y precio_val a float."""
         try:
             cantidad_val = int(cantidad_ent.get())
             precio_val = float(precio_ent.get())
@@ -53,35 +40,24 @@ def order_gui() -> dict:
         except ValueError:
             subtotal.config(text="Subtotal: 0.0")
 
-    # Bind para actualizar el valor de subtotal usando la funcion actualizar_subtotal()
+    # Bind para actualizar el subtotal cuando se cambien los valores de cantidad o precio
     cantidad_ent.bind("<KeyRelease>", actualizar_subtotal)
     precio_ent.bind("<KeyRelease>", actualizar_subtotal)
 
-    btn_final = tk.Button(window, text="FINALIZAR")
-
-    # Obtener orden y salir de la ventana
-
     def get_order() -> dict:
-        """Generar un diccionario con la informacion obtenida por los entrys.
-        si esta todo correcto retorna en forma de dict el pedido y cierra la ventana
+        """Generar un diccionario con la información obtenida de los entrys.
+        Si todo está correcto, retorna el pedido en forma de diccionario y cierra la ventana.
 
         Raises:
-            ValueError: Si no puedo transformar cantidad_ent y precio_ent
-            y si producto_val == ""
-
-        Returns:
-            dict:
-                "cantidad": cantidad_val,
-                "producto": producto_val.strip().title(),
-                "precio": precio_val,
-                "subtotal": cantidad_val * precio_val,
+            ValueError: Si no puede transformar cantidad_ent y precio_ent
+                        o si producto_val está vacío.
         """
         try:
             cantidad_val = int(cantidad_ent.get())
             precio_val = float(precio_ent.get())
             producto_val = producto_ent.get()
 
-            if producto_val == "" or producto_val.isspace():
+            if not producto_val.strip():
                 raise ValueError
 
             order = {
@@ -94,9 +70,10 @@ def order_gui() -> dict:
             window.order = order
             window.quit()
         except ValueError:
-            messagebox.showinfo("Información", "Faltan rellenar campos!")
+            messagebox.showinfo("Información", "¡Faltan rellenar campos!")
 
-    btn_final.configure(command=get_order)
+    # Botón para finalizar y obtener el pedido
+    btn_final = tk.Button(window, text="FINALIZAR", command=get_order)
     btn_final.grid(row=3, column=1)
 
     window.order = None
